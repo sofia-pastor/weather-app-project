8159ddccbe90ad2f80a54b1682c9067950d97d42
sofia-pastor <sofiapastor93@gmail.com>
2024-09-23 18:55:44 +0100

    added api and enabled search functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function updateWeatherData(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  console.log(response.data);
+}
+
+function searchCity(city) {
+  let apiKey = "tb3fa10906d8aeb018ofce8736864bb2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherData);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Porto");
