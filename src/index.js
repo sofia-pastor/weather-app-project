3837cf6abbf04167a14baac89bf3cf37f2e3db35
sofia-pastor <sofiapastor93@gmail.com>
2024-10-03 19:12:32 +0100

    Switched the outdated data for the api data
    . created formatDay();
    . updated displayForecast

--- src/index.js
+++ src/index.js
@@ -68,6 +68,13 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "tb3fa10906d8aeb018ofce8736864bb2";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -75,26 +82,29 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Fri", "Sat", "Sun", "Mon", "Thu"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">🌥</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <div>
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+    </div>
     <div class="weather-forecast-temperatures">
     <div class="weather-forecast-max-temperature">
-              <strong>15°C</strong>
+              <strong>${Math.round(day.temperature.maximum)}°C</strong>
               </div>
-              <div class="weather-forecast-min-temperature">9°C</div>
+              <div class="weather-forecast-min-temperature">${Math.round(
+                day.temperature.minimum
+              )}°C</div>
               </div>
               </div>`;
+    }
   });
 
   forecastElement.innerHTML = forecastHtml;
